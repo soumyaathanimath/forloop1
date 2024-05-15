55bdcb26b8df580085c162f7bf58c2de2838916b
soumyaathanimath <>
2024-05-15 20:45:46 +0530

    forloop

---
+++ src/forloop1/Forloop1.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package forloop1;
+
+/**
+ *
+ * @author dell1
+ */
+public class Forloop1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         //for(int i=0;i<=10;i++)
+        //for(int i=0;i>0;i--)
+        
+        //int i=0;
+        //for(System.out.println("Hi");i<=10;i++)
+        //for(;;)
+        
+        for(int i=0,j=1;i<=10;i++,j=j*2)
+        {
+            System.out.println(i);
+        }
+    }
+    
+}
